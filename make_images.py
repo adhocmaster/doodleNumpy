25f404006e0366f12f0e208ee3b887a2deb69d68
Andy <andyaport@gmail.com>
2018-12-05 17:08:13 -0800

    first commit for Andy

---
+++ make_images.py
@@ -0,0 +1,25 @@
+from __future__ import division, print_function
+import numpy as np
+import cv2 as cv
+from time import time
+import os
+
+
+npy_dir = 'NPYs'
+out_dir = 'images'
+
+os.mkdir(out_dir)
+for npy in os.listdir(npy_dir):
+    print("Working on %s ..." % npy, end='')
+    time_npy_start = time()
+    subset = os.path.splitext(npy)[0]
+    os.mkdir(os.path.join(out_dir, subset))
+
+    flat_images = np.load(os.path.join(npy_dir, npy))
+    assert flat_images.shape[1] == 28**2
+    images = flat_images.reshape(-1, 28, 28)
+
+    for k, image in enumerate(images):
+        cv.imwrite(os.path.join(out_dir, subset, subset + '_%s.png' % k), image)
+    print('Done (in %s s).' % (time() - time_npy_start))
+
