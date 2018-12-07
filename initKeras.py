60cc52ca4aceabb9be28589efe94d3de8244962a
Adhocmaster <adhocmaster@live.com>
2018-12-07 12:58:48 -0800

    models saved working on ensemble

--- initKeras.py
+++ initKeras.py
@@ -3,13 +3,16 @@ import numpy as np
 import os
 import warnings
 import logging, sys, math
+import matplotlib.pyplot as plt
 from importlib import reload
+
 from keras import models
 from keras import layers
 from keras import optimizers
 from keras import losses
 from keras import activations
 from keras import metrics
+
 from sklearn.model_selection import train_test_split
 
 logging.warning( "Keras dependencies loaded" )
